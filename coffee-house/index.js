509d2cd88238bfffbe494a146cf0235bf9433610
Nadezhda Komarova <naduha.9612@gmail.com>
2023-12-12 05:36:11 +0300

    feat: add basic slider

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,22 +1,73 @@
-let menuBtn = document.querySelector('.burger');
-let menu = document.querySelector('.menu-for-burger');
-let container = document.querySelector('.main-container');
+const menuBtn = document.querySelector(".burger");
+const menu = document.querySelector(".menu-for-burger");
+const container = document.querySelector(".main-container");
 
-let toggleMenu = function(){
-    menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-    container.classList.toggle('active');
-}
+const toggleMenu = function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+  container.classList.toggle("active");
+};
 
-menuBtn.addEventListener('click', function(event) {
-    event.stopPropagation();
-toggleMenu();
-    if (menu.classList.contains('active')) {
-        menu.addEventListener('click', function() {
-   menu.classList.remove('active');
-   menuBtn.classList.remove('active');
-   container.classList.remove('active');
-})
-        }
-}  
-);
+const sliderLine = document.querySelector(".slider-line");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const controls = document.querySelectorAll(".control");
+let width = document.querySelector('.slider-wrapper'); 
+let position = 0;
+let controlIndex = 0;
+
+const thisIndex = function (index) {
+    for (let control of controls) {
+      control.classList.remove("active");
+    }
+    controls[index].classList.add("active");
+  };
+
+const nextSlide = function () {
+  if (position < (controls.length - 1) * width.offsetWidth) {
+    position += width.offsetWidth;
+    controlIndex += 1;
+  } else {
+    position = 0;
+    controlIndex = 0;
+  }
+  sliderLine.style.left = -position + "px";
+  thisIndex(controlIndex);
+};
+
+const prevSlide = function () {
+  if (position > 0) {
+    position -= width.offsetWidth;
+    controlIndex -= 1;
+  } else {
+    position = (controls.length - 1) * width.offsetWidth;
+    controlIndex = controls.length - 1;
+  }
+  sliderLine.style.left = -position + "px";
+  thisIndex(controlIndex);
+};
+
+
+// Открыть/закрыть бургер меню
+menuBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  toggleMenu();
+  if (menu.classList.contains("active")) {
+    menu.addEventListener("click", function () {
+      menu.classList.remove("active");
+      menuBtn.classList.remove("active");
+      container.classList.remove("active");
+    });
+  }
+});
+
+//Переключить на следующий слайд
+nextButton.addEventListener("click", nextSlide);
+
+//Переключить на предыдущий слайд
+prevButton.addEventListener("click", prevSlide);
+
+//Автоматическое пролистывание слайдов 
+setInterval(() => {
+nextSlide()
+}, 5000)
