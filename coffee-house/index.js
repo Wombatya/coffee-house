e8f5af4cb0819b9894dc08abcbe6523501dfa8e1
Nadezhda Komarova <naduha.9612@gmail.com>
2023-12-12 16:27:54 +0300

    feat: add slider for mobile

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -16,6 +16,14 @@ let width = document.querySelector('.slider-wrapper');
 let position = 0;
 let controlIndex = 0;
 
+const slides = Array.from(document.querySelectorAll(".slider-item"));
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID,
+  currentIndex = 0;
+
 const thisIndex = function (index) {
     for (let control of controls) {
       control.classList.remove("active");
@@ -61,11 +69,65 @@ menuBtn.addEventListener("click", function (event) {
   }
 });
 
+if (screen.width > 380 ) {
 //Переключить на следующий слайд
 nextButton.addEventListener("click", nextSlide);
 
 //Переключить на предыдущий слайд
 prevButton.addEventListener("click", prevSlide);
+}
+
+if (screen.width <= 380) {
+    slides.forEach((slide, index) => {
+        slide.addEventListener("touchstart", touchStart(index));
+        slide.addEventListener("touchend", touchEnd);
+        slide.addEventListener("touchmove", touchMove);
+      });
+    
+      function touchStart(index) {
+        return function (event) {
+          currentIndex = index;
+          startPos = event.touches[0].clientX;
+          isDragging = true;
+        };
+      }
+      function touchMove(event) {
+        if (isDragging) {
+          const currentPosition = event.touches[0].clientX;
+          currentTranslate = prevTranslate + currentPosition - startPos;
+        }
+      }
+      function touchEnd() {
+        isDragging = false;
+        console.log(currentIndex);
+        const movedBy = currentTranslate - prevTranslate;
+        // if moved enough negative then snap to next slide if there is one
+        if (movedBy < -100) {
+            if (position < (controls.length - 1) * width.offsetWidth) {
+                position += width.offsetWidth;
+                controlIndex += 1;
+              } else {
+                position = 0;
+                controlIndex = 0;
+                currentIndex = 0;
+              }
+              sliderLine.style.left = -position + "px";
+              thisIndex(controlIndex);
+        };
+        // if moved enough positive then snap to previous slide if there is one
+        if (movedBy > 100) {
+            if (position > 0) {
+                position -= width.offsetWidth;
+                controlIndex -= 1;
+              } else {
+                position = (controls.length - 1) * width.offsetWidth;
+                controlIndex = controls.length - 1;
+              }
+              sliderLine.style.left = -position + "px";
+              thisIndex(controlIndex);
+        };
+      }
+}
 
 //Автоматическое пролистывание слайдов 
 setInterval(() => {
