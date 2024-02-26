e7e35ddc1ed207c86c5ba9b1da116b58e367fe9b
Nadezhda Komarova <naduha.9612@gmail.com>
2024-02-27 02:00:36 +0300

    feat: add api key

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '376872fc3c11491db7527f5c0397e7bf', // получите свой ключ https://newsapi.org/
         });
     }
 }
